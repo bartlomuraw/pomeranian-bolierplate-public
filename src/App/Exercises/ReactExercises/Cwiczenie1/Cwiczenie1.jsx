0aa1be45871becfbfd73e34f0472b7339802b90a
Bartlomiej Murawski <bartlo.murawski@gmail.com>
2023-07-06 19:00:23 +0200

    Dodanie zdarzenia działania aktywnego przycisku

--- src/App/Exercises/ReactExercises/Cwiczenie1/Cwiczenie1.jsx
+++ src/App/Exercises/ReactExercises/Cwiczenie1/Cwiczenie1.jsx
@@ -3,10 +3,30 @@ import React from 'react';
 import './style.css';
 
 export function Cwiczenie1() {
-  return (
-    <div>
-      <p>Ćwiczenie 1</p>
+  function handleOnSubmit(event) {
+    event.preventDefault();
+    console.log('Kliknięto przycisk');
+    console.log(event);
+  }
 
+  return (
+
+    <div className="blok-8__cwiczenie-1">
+      <h1>Cześć!</h1>
+      <div>
+        <div>
+          <p>Wpisz tekst</p>
+          <form onSubmit={handleOnSubmit}>
+            <input type="text" placeholder='tekst do wpisania' />
+            <button type="submit">KLIKNIJ</button>
+          </form>
+        </div>
+        <div>
+          <p>Placeholder</p>
+        </div>
+      </div>
     </div>
+
   );
+
 }
