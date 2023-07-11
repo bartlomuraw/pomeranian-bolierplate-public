4192c321f353603736bc13ef4ae3e61cca4e0878
Bartlomiej Murawski <bartlo.murawski@gmail.com>
2023-07-11 19:24:34 +0200

    text fundamentals2

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,10 +2,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as selectorsAndCascade } from './SelectorsAndCascade/router-data';
+import { blockRouterMetaData as TextFundamentals } from './TextFundamentals/router-data';
 
 export const blockRouterMetaData = [
     blockRouterMetaData121220231,
-    selectorsAndCascade
-
+    selectorsAndCascade,
+    TextFundamentals,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
