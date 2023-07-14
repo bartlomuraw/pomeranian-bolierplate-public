e91ef4c20098b0dd2ab226d63de5d6f5c748b172
Bartlomiej Murawski <bartlo.murawski@gmail.com>
2023-07-14 19:53:50 +0200

    css_units_wrapper

--- src/App/Exercises/HtmlCss/BoxModel/index.jsx
+++ src/App/Exercises/HtmlCss/BoxModel/index.jsx
@@ -27,6 +27,19 @@ export function BoxModel() {
         <div className="div_nr1">Przykładowy div - !conditional rendering</div>
       )}
 
+      {/*Overflow*/}
+      <div className="div_nr2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum soluta vitae debitis molestias, et blanditiis quae aliquam praesentium eum asperiores at repellendus quas maiores explicabo fuga tenetur accusantium voluptates voluptatum.
+      </div>
+      {/*CSS units*/}
+      <div clasName="css_units_wrapper">
+        <div className="div_nr1 css units_1">Przykładowy div 1</div>
+        <div className="div_nr1 css units_1">Przykładowy div 2</div>
+        <div className="div_nr1 css units_1">Przykładowy div 3</div>
+        <div className="div_nr1 css units_1">Przykładowy div 4</div>
+      </div>
+
+
     </div>
   );
 }
