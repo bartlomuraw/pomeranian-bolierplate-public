af2f0113d2c6436dccd413a1fa10441e4b971468
Bartlomiej Murawski <bartlo.murawski@gmail.com>
2023-07-17 19:15:36 +0200

    tabel_tennis

---
+++ src/App/Exercises/HtmlCss/TableTennis/index.jsx
@@ -0,0 +1,50 @@
+import './styles.css';
+
+export function TableTennis() {
+  return (
+    <div className='html-tables'>
+      <p>1. The following data were collected...</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Location</th>
+            <th>Player_ID</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Cape Verde Islands</td>
+            <td>#100120</td>
+            <td>Nazwa</td>
+
+          </tr>
+          <tr>
+            <td>Cape Verde Islands</td>
+            <td>#100120</td>
+            <td>Nazwa</td>
+
+          </tr>
+          <tr>
+            <td>Cape Verde Islands</td>
+            <td>#100120</td>
+            <td>Nazwa</td>
+
+          </tr>
+          <tr>
+            <td>United States of America</td>
+            <td>#100120</td>
+            <td>Nazwa</td>
+
+          </tr>
+          <tr>
+            <td>United States of America</td>
+            <td>#100120</td>
+            <td>Nazwa</td>
+
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
---
+++ src/App/Exercises/HtmlCss/TableTennis/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { TableTennis } from './index';
+
+export const blockRouterMetaData = {
+  path: 'table-tennis',
+  date: '17-07-2023',
+  linkLabel: 'TableTennis',
+  blockNo: 1,
+  element: <TableTennis />,
+  tags: ['flex', 'grid', 'video'],
+};
