12a1fe2b2d5b5edbcd632d5cddc22d1dd9b84ff9
Bartlomiej Murawski <bartlo.murawski@gmail.com>
2023-07-17 17:37:43 +0200

    tables

---
+++ src/App/Exercises/HtmlCss/Tables/index.jsx
@@ -0,0 +1,57 @@
+import './styles.css';
+
+export const Tables = () => {
+  return (
+    <div className='html-tables'>
+      <p>1. The following data were collected...</p>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age1</th>
+            <th>Age2</th>
+            <th>Age2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>ID</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+          </tr>
+          <tr>
+            <td>ID</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+          </tr>
+          <tr>
+            <td>ID</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+          </tr>
+          <tr>
+            <td>ID</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+          </tr>
+          <tr>
+            <td>ID</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+            <td>Nazwa</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+};
