b367c16e32b67bded210887853e160bdc74fccf5
Bartlomiej Murawski <bartlo.murawski@gmail.com>
2023-07-17 17:06:43 +0200

    FAQ1

--- src/App/FAQ/index.jsx
+++ src/App/FAQ/index.jsx
@@ -1,5 +1,60 @@
 import './styles.css';
 
-export const FAQ = () => {
-  return <div>Change me</div>;
+export function FAQ() {
+  return (
+    <div className="faq-wrapper">
+      <p className="faq-pointer">FAQ</p>
+      <p>Tutaj znajdź odpowiedzi na najczęściej zadawane pytania</p>
+
+      <article className="faq-content">
+        <h3 className="faq-question">
+          Jak mogę zapisać się na szkolenie?
+        </h3>
+
+        <p className="faq-answer">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque obcaecati nam,
+          omnis voluptatem numquam cupiditate, illum esse rem consequuntur dolor consequatur
+          deleniti nisi architecto quos repellendus! Vero quidem molestias ad.
+        </p>
+
+      </article>
+      <article className="faq-content">
+        <h3 className="faq-question">
+          Jak mogę zapisać się na szkolenie?
+        </h3>
+
+        <p className="faq-answer">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque obcaecati nam,
+          omnis voluptatem numquam cupiditate, illum esse rem consequuntur dolor consequatur
+          deleniti nisi architecto quos repellendus! Vero quidem molestias ad.
+        </p>
+
+      </article>
+      <article className="faq-content">
+        <h3 className="faq-question">
+          Jak mogę zapisać się na szkolenie?
+        </h3>
+
+        <p className="faq-answer">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque obcaecati nam,
+          omnis voluptatem numquam cupiditate, illum esse rem consequuntur dolor consequatur
+          deleniti nisi architecto quos repellendus! Vero quidem molestias ad.
+        </p>
+
+      </article>
+      <article className="faq-content">
+        <h3 className="faq-question">
+          Jak mogę zapisać się na szkolenie?
+        </h3>
+
+        <p className="faq-answer">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque obcaecati nam,
+          omnis voluptatem numquam cupiditate, illum esse rem consequuntur dolor consequatur
+          deleniti nisi architecto quos repellendus! Vero quidem molestias ad.
+        </p>
+
+      </article>
+
+    </div>
+  )
 };
