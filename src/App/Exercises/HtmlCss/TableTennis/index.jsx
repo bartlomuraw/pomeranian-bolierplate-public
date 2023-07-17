e43922cfc9dbc7734034d606049d3b1fb2a93711
Bartlomiej Murawski <bartlo.murawski@gmail.com>
2023-07-17 19:28:33 +0200

    table_tennis2

--- src/App/Exercises/HtmlCss/TableTennis/index.jsx
+++ src/App/Exercises/HtmlCss/TableTennis/index.jsx
@@ -1,50 +1,77 @@
+import { StarIcon } from '../../../Components/Icons/StarIcon';
+
 import './styles.css';
 
 export function TableTennis() {
-  return (
-    <div className='html-tables'>
-      <p>1. The following data were collected...</p>
-      <table>
-        <thead>
-          <tr>
-            <th>Location</th>
-            <th>Player_ID</th>
-            <th>Rating</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Cape Verde Islands</td>
-            <td>#100120</td>
-            <td>Nazwa</td>
 
-          </tr>
-          <tr>
-            <td>Cape Verde Islands</td>
-            <td>#100120</td>
-            <td>Nazwa</td>
+  return <div className="table-tennis">
 
-          </tr>
-          <tr>
-            <td>Cape Verde Islands</td>
-            <td>#100120</td>
-            <td>Nazwa</td>
+    <table>
 
-          </tr>
-          <tr>
-            <td>United States of America</td>
-            <td>#100120</td>
-            <td>Nazwa</td>
+      <tr>
+        <th>Location</th>
+        <th>PLAYER_ID</th>
+        <th>Rating</th>
+      </tr>
 
-          </tr>
-          <tr>
-            <td>United States of America</td>
-            <td>#100120</td>
-            <td>Nazwa</td>
+      <tr>
+        <td>Cape Verde Islands</td>
+        <td>#100120</td>
+        <td>
+          <StarIcon />
+          <StarIcon />
+        </td>
 
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-};
+      </tr>
+
+      <tr>
+        <td>Cape Verde Islands</td>
+        <td>#100122</td>
+        <td>
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+        </td>
+
+      </tr>
+
+      <tr>
+        <td>Cape Verde Islands</td>
+        <td>#100124</td>
+        <td>
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+        </td>
+
+      </tr>
+
+      <tr>
+        <td>United States of America</td>
+        <td>#100126</td>
+        <td>
+          <StarIcon />
+        </td>
+      </tr>
+
+      <tr>
+
+        <td>United States of America</td>
+        <td>#100128</td>
+        <td>
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+        </td>
+
+      </tr>
+
+    </table>
+
+  </div>;
+
+}
