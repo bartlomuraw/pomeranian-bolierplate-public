21fd1519d82c56c006f7221ff653397514101e85
Bartlomiej Murawski <bartlo.murawski@gmail.com>
2023-07-18 19:27:03 +0200

    header_info

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -3,15 +3,34 @@ import React from 'react';
 import './styles/header.css';
 
 import { Logo } from '../Components/Logo/Logo';
+import { SettingIcon } from '../Components/Icons/SettingIcon';
+import ToggleArrow from '../Images/toggle-arrow.svg';
 
 export function AppHeader() {
+  function handleClickButton() {
+    console.log('Button clicked')
+  }
   return (
     <header>
-      <div>
-        {/* <Logo></Logo> */}
-        <Logo />
-        <div className="header-title"></div>
+      <Logo />
+      <div className="header-title">
+        <div>
+          <button onClick={() => handleClickButton('I am settings button')}>
+            <SettingIcon />
+          </button>
+        </div>
+        <div className='header-personal-info'>
+          <p>Bartek</p>
+          <p>Kursant</p>
+        </div>
+        <div className="toggle-arrow">
+          <buttno onClick={() => handleClickButton('I am toggle button')}>
+            <img src={ToggleArrow} alt="toggle-arrow" />
+          </buttno>
+        </div>
+
+
       </div>
-    </header>
+    </header >
   );
 }
