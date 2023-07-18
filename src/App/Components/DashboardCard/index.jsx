f863c6c03167cd6fdbd9403ee2546e2d7c6fbcbd
Bartlomiej Murawski <bartlo.murawski@gmail.com>
2023-07-18 23:03:42 +0200

    dashboard1

---
+++ src/App/Components/DashboardCard/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+export const DashboardCard = (props) => {
+    return (
+        <div className="dashboard-card-wrapper">
+            <p className="dashboard-card-title">{props.title}</p>
+            {props.icon}
+            <p className="dashboard-card-description">{props.description}</p>
+            <a href="#" className="dashboard-card-navlink">
+                zobacz więcej
+            </a>
+        </div>
+    );
+};
