30dbf4f541bb390e4e5238a31f2af0ba8da2ff1b
Bartlomiej Murawski <bartlo.murawski@gmail.com>
2023-07-20 19:54:38 +0200

    cookiebanner1

---
+++ src/App/Components/CookieBanner/CookieBanner.jsx
@@ -0,0 +1,61 @@
+import './styles.css';
+import cookieBanner from './cookie.png';
+
+
+/*function setCookieState() {
+  localStorage.setItem("cookieState", 1);
+}
+
+function getCookieState() {
+  return localStorage.getItem("cookieState");
+}
+
+export const CookieBanner = () => {
+  const currentBannerState = getCookieState();
+  const [showBanner, setShowBanner] = useState(!currentBannerState)
+
+  function hideBanner() {
+    setShowBanner(0)
+    setCookieState()
+  }
+
+  if (!showBanner) {
+    return null;
+  }
+
+  return <div id="cookie-banner">
+    <div id="cookie-banner-overlay"></div>
+    <div id="cookie-banner-content">
+      <button onClick={hideBanner}>GIT</button>
+    </div>
+  </div>;
+};*/
+
+
+
+
+export const CookieBanner = () => {
+  return (
+    <div>
+      <div className='cookie-banner-overlay'>
+
+      </div>
+      <div className='cookie-banner'>
+        <img className="floats-img-cookie" src={cookieBanner} alt="cookie" />
+        <div className='cookie-use'>
+          <p>
+            <div className='cookie-banner-let'>
+              <h2>Pozwól na pliki cookies</h2>
+            </div>
+            Nasza strona korzysta z ciasteczek, które umożliwiają poprawne działanie strony i pomagają nam świadczyć usługi na najwyższym poziomie. Możesz zaakceptować wykorzystanie przez nas wszystkich tych plików i przejść do strony lub dostosowaćz użycie do swoich preferencji. W tym celu kliknij przycisk po prawej stronie “dopasuj zgody”, aby następnie wybrać te które odpowiadają Twoim indywidualnym preferencjom.
+          </p>
+          <div className='cookie-banner-buttons'>
+            <button className='cookie-banner-button1'>W PORZĄDKU</button>
+            <button className='cookie-banner-button2'>DOPASUJ ZGODY</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+};
