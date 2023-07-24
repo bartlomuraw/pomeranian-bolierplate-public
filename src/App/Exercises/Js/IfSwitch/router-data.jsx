93a0fe524edffca2bd7a5d5bf89c0ca1d7f167d1
Bartlomiej Murawski <bartlo.murawski@gmail.com>
2023-07-24 19:32:20 +0200

    ifswitch

---
+++ src/App/Exercises/Js/IfSwitch/router-data.jsx
@@ -0,0 +1,10 @@
+import { IfSwitch } from './index';
+
+export const blockRouterMetaData = {
+  path: 'if-switch',
+  date: null,
+  linkLabel: 'IfSwitch ćwiczenia',
+  blockNo: 6,
+  element: <IfSwitch />,
+  tags: ['if', 'switch', 'let', 'const', 'true', 'false'],
+};
