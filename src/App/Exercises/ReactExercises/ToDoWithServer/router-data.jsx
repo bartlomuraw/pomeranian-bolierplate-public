a6a1dbf922bfbc1e74b619c1a99fdb6b45db68cc
Bartlomiej Murawski <bartlo.murawski@gmail.com>
2023-08-28 12:09:55 +0200

    to_do_przygotownie

---
+++ src/App/Exercises/ReactExercises/ToDoWithServer/router-data.jsx
@@ -0,0 +1,10 @@
+import { ToDoWithServer } from './ToDoWithServer';
+
+export const routerMetaData = {
+  path: 'todo-with-server',
+  date: '21-08-2023',
+  linkLabel: 'Todo z serwerem',
+  blockNo: 36,
+  element: <ToDoWithServer />,
+  tags: ['todo', 'fetch', 'rest', 'api', 'javascript'],
+};
